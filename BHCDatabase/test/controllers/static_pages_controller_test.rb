1f41edb18767574fe19aaa81b377fb185e37de62
David Robertson <2135967r@bo720-1-02u.dcs.gla.ac.uk>
2016-12-02 11:08:38 +0000

    Set up layout_page

--- BHCDatabase/test/controllers/static_pages_controller_test.rb
+++ BHCDatabase/test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Building Healthy Communities"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Building Healthy Communities"
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
