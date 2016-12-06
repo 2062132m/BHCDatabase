659916f8d83670f24c73a8dc6032acf06eff0d88
DaBrown95 <david_a_brown@mac.com>
2016-12-06 16:59:31 +0000

    Fix tests to cooperate with the new style of login page

--- BHCDatabase/test/integration/users_login_test.rb
+++ BHCDatabase/test/integration/users_login_test.rb
@@ -10,6 +10,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
+    follow_redirect!
     assert_template 'sessions/new'
     assert_not flash.empty?
     get login_path
@@ -45,7 +46,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # Simulate a user clicking logout in a second window.
     delete logout_path
     follow_redirect!
-    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", login_path,       count: 0
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
