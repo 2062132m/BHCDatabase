4163366bce760355d01164b1d740f4ff1e8a3c66
DaBrown95 <david_a_brown@mac.com>
2016-12-06 17:47:53 +0000

    Write tests to complete issue #28

--- BHCDatabase/test/integration/site_layout_test.rb
+++ BHCDatabase/test/integration/site_layout_test.rb
@@ -4,10 +4,28 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:david)
-    log_in_as(@user, remember_me: '1')
   end
 
-  test "layout links" do
+  test "layout links when logged out" do
+    get root_path
+    follow_redirect!
+    assert_template 'sessions/new'
+    assert_select "a[href=?]", login_path,       count: 0
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", root_url,         count: 0
+    get contact_path
+    assert_template 'static_pages/contact'
+    assert_select "title", full_title("Contact")
+    assert_select "a[href=?]", login_path,       count: 1
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", root_url,         count: 0
+
+  end
+
+  test "layout links when logged in" do
+    log_in_as(@user, remember_me: '1')
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 3
