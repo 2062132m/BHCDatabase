8ad8569b4fafec7d2475330a2f5a45e025062df9
DaBrown95 <david_a_brown@mac.com>
2017-01-12 16:01:32 +0000

    Add test for reaching the 'new' area page

--- BHCDatabase/test/controllers/areas_controller_test.rb
+++ BHCDatabase/test/controllers/areas_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class AreasControllerTest < ActionDispatch::IntegrationTest
-  # test "should get new" do
-  #   get areas_new_url
-  #   assert_response :success
-  # end
+
+  def setup
+    @user = users(:david)
+    log_in_as(@user)
+  end
+
+  test "should get new" do
+    get areas_new_url
+    assert_response :success
+  end
 
 end
