fd5876e5949c3c2e7e68d9fd8c7d26deea05207d
David Robertson <dmichaelrobertson@gmail.com>
2017-01-17 10:58:31 +0000

    Modify seeds.rb to insert a User account for all 6 developers along with the 4 initial areas into the database. Use rails db:seed.

--- BHCDatabase/db/seeds.rb
+++ BHCDatabase/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "David Robertson", email: "david@david.com", password:
+"david123", password_confirmation: "david123")
+User.create(name: "David Brown", email: "david@david.com", password:
+"david123", password_confirmation: "david123")
+User.create(name: "Maria Papadopoulou", email: "maria@maria.com", password:
+"maria123", password_confirmation: "maria123")
+User.create(name: "Jaklin Yordanova", email: "jaklin@jaklin.com", password:
+"jaklin123", password_confirmation: "jaklin123")
+User.create(name: "Chris Harris", email: "chris@chris.com", password:
+"chris123", password_confirmation: "chris123")
+User.create(name: "Kiril Mihaylov", email: "kiril@kiril.com", password:
+"kiril123", password_confirmation: "kiril123")
+
+Area.create(name: "Dumfries & Lower Nithsdale", description: "blah")
+Area.create(name: "West Wigtownshire", description: "blah")
+Area.create(name: "The Machars", description: "blah")
+Area.create(name: "Upper Nithsdale", description: "blah")
