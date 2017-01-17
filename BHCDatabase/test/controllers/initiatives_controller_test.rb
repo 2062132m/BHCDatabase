06f2d4d85ed8fd3fc057c6bb00bc6d4d13b80199
David Robertson <dmichaelrobertson@gmail.com>
2017-01-17 13:05:37 +0000

    Fix controller test.

--- BHCDatabase/test/controllers/initiatives_controller_test.rb
+++ BHCDatabase/test/controllers/initiatives_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class InitiativesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:david)
+    log_in_as(@user)
+  end
+
   test "should get new" do
     get initiatives_new_url
     assert_response :success
