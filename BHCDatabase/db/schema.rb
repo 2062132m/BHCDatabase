8f1bc8e6a2d2fef65a4b111ccba6e67e1bd09a15
David Robertson <dmichaelrobertson@gmail.com>
2017-01-17 13:17:59 +0000

    Add model tests for initiatives.

--- BHCDatabase/db/schema.rb
+++ BHCDatabase/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112152901) do
+ActiveRecord::Schema.define(version: 20170117131315) do
 
   create_table "areas", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "initiatives", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
