1b55acc8de2f644028e7a32709a5c1201373033c
David Robertson <dmichaelrobertson@gmail.com>
2017-01-17 13:24:41 +0000

    Create index page for initiatives.

--- BHCDatabase/app/controllers/initiatives_controller.rb
+++ BHCDatabase/app/controllers/initiatives_controller.rb
@@ -1,4 +1,13 @@
 class InitiativesController < ApplicationController
+
+  def index
+    @initiatives = Initiative.all
+  end
+
+  def show
+    @initiatives = Initiative.find(params[:id])
+  end
+
   def new
   end
 end
