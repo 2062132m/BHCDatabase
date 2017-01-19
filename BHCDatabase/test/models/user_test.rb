366d33c06a5f87fd0c4533c301408717695146a0
David Robertson <dmichaelrobertson@gmail.com>
2017-01-19 17:05:09 +0000

    Added a few validation tests for each field.

--- BHCDatabase/test/models/user_test.rb
+++ BHCDatabase/test/models/user_test.rb
@@ -23,6 +23,20 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "telephone should be present" do
+    @user.telephone = ""
+    assert_not @user.valid?
+  end
+
+  test "dob should be present" do
+    @user.dob = ""
+    assert_not @user.valid?
+  end
+
+  test "privilege should be present" do
+    @user.privilege = ""
+    assert_not @user.valid?
+  end
 
 test "name should not be too long" do
     @user.name = "a" * 51
@@ -31,6 +45,11 @@ test "name should not be too long" do
 
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "telephone should not be too long" do
+    @user.telephone = "1" * 17
     assert_not @user.valid?
   end
 
