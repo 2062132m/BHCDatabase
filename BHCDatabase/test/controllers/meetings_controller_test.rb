e11e751582e202d6c3e55e9a75304769b78b0541
David Robertson <dmichaelrobertson@gmail.com>
2017-01-23 13:02:03 +0000

    Fix meetings controller test.

--- BHCDatabase/test/controllers/meetings_controller_test.rb
+++ BHCDatabase/test/controllers/meetings_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class MeetingsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:david)
+    log_in_as(@user)
+  end
+
   test "should get new" do
     get meetings_new_url
     assert_response :success
