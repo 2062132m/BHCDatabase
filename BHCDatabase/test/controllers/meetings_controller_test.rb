8b4e00832c541f34b173b9b5f9567107360c9a15
David Robertson <dmichaelrobertson@gmail.com>
2017-01-23 13:08:48 +0000

    Add show page for meetings and removed new page. Removed test for should_get_new.

--- BHCDatabase/test/controllers/meetings_controller_test.rb
+++ BHCDatabase/test/controllers/meetings_controller_test.rb
@@ -7,9 +7,9 @@ class MeetingsControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
   end
 
-  test "should get new" do
-    get meetings_new_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get meetings_new_url
+  #   assert_response :success
+  # end
 
 end
