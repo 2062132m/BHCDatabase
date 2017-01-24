ddc2a67f5864ce8bb2df1ec3b28183411456d478
David Robertson <dmichaelrobertson@gmail.com>
2017-01-24 14:13:11 +0000

    ID and name now link to the correct initative.

--- BHCDatabase/app/controllers/initiatives_controller.rb
+++ BHCDatabase/app/controllers/initiatives_controller.rb
@@ -25,8 +25,16 @@ class InitiativesGrid
     Initiative
   end
 
-  column(:id, :mandatory => true)
-  column(:name, :mandatory => true)
+  column(:id, :mandatory => true) do |model|
+    format(model.id) do |value|
+      link_to value, model
+    end
+  end
+  column(:name, :mandatory => true) do |model|
+    format(model.name) do |value|
+      link_to value, model
+    end
+  end
   column(:location, :mandatory => true)
 
 end
