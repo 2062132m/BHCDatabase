c61dddf3ddd9a6d677669cb1a117ac232395643f
David Robertson <dmichaelrobertson@gmail.com>
2017-01-25 18:12:56 +0000

    Add grid for meetings to initatives show.

--- BHCDatabase/app/controllers/initiatives_controller.rb
+++ BHCDatabase/app/controllers/initiatives_controller.rb
@@ -8,9 +8,13 @@ class InitiativesController < ApplicationController
   end
 
   def show
-    @initiative = Initiative.find(params[:id])
-    @area = Area.find(@initiative.area_id)
-    @meetings = Meeting.where(initiative_id: @initiative)
+    $initiative = Initiative.find(params[:id])
+    @area = Area.find($initiative.area_id)
+    @meetings = Meeting.where(initiative_id: $initiative)
+    @meetings_in_initiatives_grid = MeetingsInInitiativesGrid.
+    new(params[:meetings_in_initiatives_grid]) do |scope|
+      scope.page(params[:page])
+    end
   end
 
   def new
@@ -42,6 +46,27 @@ class InitiativesGrid
   column(:area_id, :header => "Area", :mandatory => true) do |model|
     format(model.area_id) do |value|
       Area.find(value).name
+    end
+  end
+
+end
+
+class MeetingsInInitiativesGrid
+
+  include Datagrid
+
+  scope do
+    Meeting.where(initiative_id: $initiative)
+  end
+
+  column(:id, :mandatory => true) do |model|
+    format(model.id) do |value|
+      link_to value, model
+    end
+  end
+  column(:datetime, :mandatory => true) do |model|
+    format(model.datetime) do |value|
+      link_to value, model
     end
   end
 
