499aad7b3600f0d27720b9cf498112c45691b6d8
David Robertson <dmichaelrobertson@gmail.com>
2017-01-31 14:38:53 +0000

    Make meetings validate initative_id and add several tests to ensure validity of meetings models.

--- BHCDatabase/test/models/meeting_test.rb
+++ BHCDatabase/test/models/meeting_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class MeetingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @area = Area.create(name: "Example Area", description: "Example description")
+    @initiative = Initiative.create(name: "Example Initiative",
+    description: "Example description.", area_id: @area.id, location: "TestTest")
+    @meeting = @initiative.meetings.create(datetime: Time.now)
+  end
+
+  test "should be valid" do
+    assert @meeting.valid?
+  end
+
+  test "datetime should be present" do
+    @meeting.datetime = nil
+    assert_not @meeting.valid?
+  end
+
+  test "initiative_id should be present" do
+    @meeting.initiative_id = nil
+    assert_not @meeting.valid?
+  end
+
+  test "datetime should accept valid dates" do
+    valid_date = "01/01/2012 23:59:59"
+    @meeting.datetime = valid_date
+    assert @meeting.datetime.to_datetime.is_a?(DateTime)
+  end
+
+  test "datetime should reject invalid dates" do
+    invalid_date = "12/13/2016"
+    @meeting.datetime = invalid_date
+    assert_raises(Exception){@meeting.datetime.to_datetime}
+  end
+
 end
