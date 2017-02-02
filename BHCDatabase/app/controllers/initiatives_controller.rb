d967bb6f0758a07cbe360cd2c746a00761702235
David Robertson <dmichaelrobertson@gmail.com>
2017-02-02 16:12:44 +0000

    Move meetings grid into grid folder. Fix more global variables.

--- BHCDatabase/app/controllers/initiatives_controller.rb
+++ BHCDatabase/app/controllers/initiatives_controller.rb
@@ -8,36 +8,15 @@ class InitiativesController < ApplicationController
   end
 
   def show
-    $initiative = Initiative.find(params[:id])
-    @area = Area.find($initiative.area_id)
-    @meetings = Meeting.where(initiative_id: $initiative)
+    @initiative = Initiative.find(params[:id])
+    @area = Area.find(@initiative.area_id)
+    @meetings = Meeting.where(initiative_id: @initiative)
     @meetings_in_initiatives_grid = MeetingsInInitiativesGrid.
     new(params[:meetings_in_initiatives_grid]) do |scope|
-      scope.page(params[:page])
+      scope.where(:initiative_id => @initiative).page(params[:page])
     end
   end
 
   def new
   end
-end
-
-class MeetingsInInitiativesGrid
-
-  include Datagrid
-
-  scope do
-    Meeting.where(initiative_id: $initiative)
-  end
-
-  column(:id, :mandatory => true) do |model|
-    format(model.id) do |value|
-      link_to value, model
-    end
-  end
-  column(:datetime, :mandatory => true) do |model|
-    format(model.datetime) do |value|
-      link_to value.strftime("%B %d, %Y - %H:%M"), model
-    end
-  end
-
 end
--- BHCDatabase/app/grids/initiatives_grid.rb
+++ BHCDatabase/app/grids/initiatives_grid.rb
@@ -27,3 +27,24 @@ class InitiativesGrid
   end
 
 end
+
+class MeetingsInInitiativesGrid
+
+  include Datagrid
+
+  scope do
+    Meeting
+  end
+
+  column(:id, :mandatory => true) do |model|
+    format(model.id) do |value|
+      link_to value, model
+    end
+  end
+  column(:datetime, :mandatory => true) do |model|
+    format(model.datetime) do |value|
+      link_to value.strftime("%B %d, %Y - %H:%M"), model
+    end
+  end
+
+end
