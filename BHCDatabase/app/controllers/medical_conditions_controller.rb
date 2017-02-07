4b246e716ea827980da1c758737862baf982fd12
DaBrown95 <david_a_brown@mac.com>
2017-02-07 12:34:44 +0000

    Generate controller and pages for medical conditions.

---
+++ BHCDatabase/app/controllers/medical_conditions_controller.rb
@@ -0,0 +1,10 @@
+class MedicalConditionsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ BHCDatabase/test/controllers/medical_conditions_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MedicalConditionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get medical_conditions_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get medical_conditions_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get medical_conditions_show_url
+    assert_response :success
+  end
+
+end
