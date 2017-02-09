89ca369e193acbeee926567f39fd6a3a5e6a8cc0
DaBrown95 <david_a_brown@mac.com>
2017-02-09 18:04:18 +0000

    Change initiative new form.

--- BHCDatabase/test/controllers/enrolments_controller_test.rb
+++ BHCDatabase/test/controllers/enrolments_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class EnrolmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get enrolments_index_url
-    assert_response :success
-  end
-
 end
