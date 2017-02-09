dcc51fe81cc1818247cb6b8101baaace8de5945b
DaBrown95 <david_a_brown@mac.com>
2017-02-09 17:57:58 +0000

    Add functionality to enrol a user in an initiative

--- BHCDatabase/app/controllers/enrolments_controller.rb
+++ BHCDatabase/app/controllers/enrolments_controller.rb
@@ -4,4 +4,29 @@ class EnrolmentsController < ApplicationController
       scope.page(params[:page])
     end
   end
+
+  def new
+    @enrolment = Enrolment.new
+    @initiatives = Initiative.all
+    @users = User.all
+  end
+
+  def create
+    @enrolment = Enrolment.new(enrolment_params)
+    @initiatives = Initiative.all
+    @users = User.all
+    if @enrolment.save
+      flash[:success] = 'Created the new area!'
+      redirect_to @enrolment.user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def enrolment_params
+    params.require(:enrolment).permit(:user_id, :initiative_id)
+  end
+
 end
