6303a1442fe71db5e8d676dc6efb5fe1522409a7
DaBrown95 <david_a_brown@mac.com>
2017-02-14 13:00:17 +0000

    Add edit and new functionality to a question.

--- BHCDatabase/app/controllers/questions_controller.rb
+++ BHCDatabase/app/controllers/questions_controller.rb
@@ -12,4 +12,39 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:success] = 'Created the new question!'
+      redirect_to @question
+    else
+      render 'new'
+    end
+
+    def edit
+      @question = Question.find(params[:id])
+    end
+
+    def update
+      @question = Question.find(params[:id])
+      if @question.update_attributes(question_params)
+        flash[:success] = 'Question updated'
+        redirect_to @question
+      else
+        render 'edit'
+      end
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:question, :sort, :visible, :multiple_choice)
+  end
+
 end
