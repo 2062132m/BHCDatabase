e32d5b34c66d13f37655ea54f0f93c26ef9c6d69
DaBrown95 <david_a_brown@mac.com>
2017-02-16 10:51:29 +0000

    Generate basic controller and index. Add route for the page.

--- BHCDatabase/config/routes.rb
+++ BHCDatabase/config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get     '/volunteershome', to: 'volunteers_homepage#index'
   resources :users, :areas, :initiatives, :meetings, :medical_conditions, :enrolments, :questions
 end
