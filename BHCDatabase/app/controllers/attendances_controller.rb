49dd3f8477c5e36c22c0023043509e89f133da47
David Robertson <dmichaelrobertson@gmail.com>
2017-02-17 16:13:28 +0000

    Fix bug where form wouldn't submit if no members selected.

--- BHCDatabase/app/controllers/attendances_controller.rb
+++ BHCDatabase/app/controllers/attendances_controller.rb
@@ -4,18 +4,22 @@ class AttendancesController < ApplicationController
 
   def create
     meeting_id = 0
-    params[:attendance].each do |a|
-      words = a.split(',')
-      @attendance = Attendance.new(:user_id => words[0].scan( /\d+$/ ).first,
-                                   :meeting_id => words[1][0...-1].scan( /\d+$/ ).first)
-      unless @attendance.save
-        flash[:danger] = "Something went wrong."
-        redirect_to Meeting.find(meeting_id)
+    unless params[:attendance].nil?
+      params[:attendance].each do |a|
+        words = a.split(',')
+        @attendance = Attendance.new(:user_id => words[0].scan( /\d+$/ ).first,
+                                     :meeting_id => words[1][0...-1].scan( /\d+$/ ).first)
+        unless @attendance.save
+          flash[:danger] = "Something went wrong."
+          redirect_to Meeting.find(meeting_id)
+        end
+        meeting_id = words[1][0...-1].scan( /\d+$/ ).first
       end
-      meeting_id = words[1][0...-1].scan( /\d+$/ ).first
-    end
     flash[:success] = "Attendance saved!"
     redirect_to Meeting.find(meeting_id)
+    end
+    flash[:success] = "Please select at least one attendant"
+    redirect_to(:back)
   end
 
   # private
