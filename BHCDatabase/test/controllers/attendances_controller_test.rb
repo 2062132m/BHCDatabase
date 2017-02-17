1c3b31afb3077643b143f60f26f55e9ef3020aa9
David Robertson <dmichaelrobertson@gmail.com>
2017-02-17 17:14:13 +0000

    Fix test.

--- BHCDatabase/test/controllers/attendances_controller_test.rb
+++ BHCDatabase/test/controllers/attendances_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class AttendancesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @user = users(:david)
-    log_in_as(@user)
-  end
-
-  test "should get new" do
-    get attendances_new_url
-    assert_response :success
-  end
 
 end
