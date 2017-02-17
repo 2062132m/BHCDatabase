865d9cd2d3fafd4ffe02a86eb775e6afa43f69f5
DaBrown95 <david_a_brown@mac.com>
2017-02-17 13:09:55 +0000

    Add ability to add a new feedback to the database.

--- BHCDatabase/app/controllers/feedbacks_controller.rb
+++ BHCDatabase/app/controllers/feedbacks_controller.rb
@@ -1,5 +1,30 @@
 class FeedbacksController < ApplicationController
+
+  skip_before_action :admin_only
+
   def show
     @feedback = Feedback.find(params[:id])
+  end
+
+  def new
+    @feedback = Feedback.new
+    @users = User.all
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    @users = User.all
+    if @feedback.save
+      flash[:success] = 'Created a new feedback!'
+      redirect_to @feedback
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:user_id)
   end
 end
