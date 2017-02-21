53814e44591c49bd57be87b93368e3c8fd51bb03
DaBrown95 <david_a_brown@mac.com>
2017-02-21 10:33:55 +0000

    Reformat pages and correct missing </div>

--- BHCDatabase/app/controllers/attendances_controller.rb
+++ BHCDatabase/app/controllers/attendances_controller.rb
@@ -8,32 +8,26 @@ class AttendancesController < ApplicationController
     unless params[:attendance].nil?
       params[:attendance].each do |a|
         words = a.split(',')
-        @attendance = Attendance.new(:user_id => words[0].scan( /\d+$/ ).first,
-                                     :meeting_id => words[1][0...-1].scan( /\d+$/ ).first)
+        @attendance = Attendance.new(:user_id => words[0].scan(/\d+$/).first,
+                                     :meeting_id => words[1][0...-1].scan(/\d+$/).first)
         unless @attendance.save
           flash[:danger] = "Something went wrong."
           redirect_to Meeting.find(meeting_id)
           return
         end
         @numAttendees += 1
-        meeting_id = words[1][0...-1].scan( /\d+$/ ).first
+        meeting_id = words[1][0...-1].scan(/\d+$/).first
       end
-    meeting = Meeting.find(meeting_id)
-    @initiative = meeting.initiative
-    @totalAttendees = Enrolment.where(:initiative_id => @initiative).count
-    meeting.update_attribute(:attendance, @numAttendees/@totalAttendees.to_f * 100)
-    meeting.save
-    flash[:success] = "Attendance saved!"
-    redirect_to Meeting.find(meeting_id)
-    return
+      meeting = Meeting.find(meeting_id)
+      @initiative = meeting.initiative
+      @totalAttendees = Enrolment.where(:initiative_id => @initiative).count
+      meeting.update_attribute(:attendance, @numAttendees/@totalAttendees.to_f * 100)
+      meeting.save
+      flash[:success] = "Attendance saved!"
+      redirect_to Meeting.find(meeting_id)
+      return
     end
     flash[:danger] = "Please select at least one attendant"
     redirect_to(:back)
   end
-
-  # private
-  #
-  #   def attendance_params
-  #     params.permit(attendance:[])
-  #   end
 end
