27a964d352ad98466884049bd53f2583ca25336b
DaBrown95 <david_a_brown@mac.com>
2017-02-21 14:26:10 +0000

    Sort feedback on the #show page.

--- BHCDatabase/app/models/answer.rb
+++ BHCDatabase/app/models/answer.rb
@@ -4,4 +4,9 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :response, presence: true, length: {maximum: 255}
+
+  def <=>(other)
+    self.question <=> other.question
+  end
+
 end
