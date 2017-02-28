46ad863c3a95814583fc6890e65ab6bf69244170
Kiril <smuhrin@gmail.com>
2017-02-28 16:29:19 +0000

    added edit button, changes to routes, controlelr and new/form views

--- BHCDatabase/app/controllers/service_requests_controller.rb
+++ BHCDatabase/app/controllers/service_requests_controller.rb
@@ -1,2 +1,24 @@
 class ServiceRequestsController < ApplicationController
+  def show
+    @service_request = ServiceRequest.find(params[:id])
+  end
+
+  def new
+    @user = @current_user
+    @service_request = ServiceRequest.new
+    end
+
+  def create
+    @user = @current_user
+    @service_request  = ServiceRequest.new(service_request_params)
+
+  end
+
+
+
+  def service_request_params
+    params.require(:service_request).permit(:user_id, :request)
+  end
+
+
 end
