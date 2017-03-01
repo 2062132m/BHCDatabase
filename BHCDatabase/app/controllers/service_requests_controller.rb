bdaf2bb2d6eb464462e221652e573292bb121e09
Kiril <smuhrin@gmail.com>
2017-03-01 16:43:15 +0000

    fixed form size, privilages, and tweeks to controller

--- BHCDatabase/app/controllers/service_requests_controller.rb
+++ BHCDatabase/app/controllers/service_requests_controller.rb
@@ -1,4 +1,5 @@
 class ServiceRequestsController < ApplicationController
+  skip_before_action :admin_only
   def show
     @service_request = ServiceRequest.find(params[:id])
   end
@@ -11,10 +12,17 @@ class ServiceRequestsController < ApplicationController
   def create
     @user = @current_user
     @service_request  = ServiceRequest.new(service_request_params)
+    if @service_request.save
+      flash[:success] = 'Created a new service request!'
+      redirect_to @current_user
+    else
+      flash[:danger] = 'Oh no!'
+      redirect_to @current_user
+
+    end
+
 
   end
-
-
 
   def service_request_params
     params.require(:service_request).permit(:user_id, :request)
