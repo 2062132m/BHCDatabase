7c97cc978beea4f66299f3c053120379a17aa1ec
DaBrown95 <david_a_brown@mac.com>
2017-03-02 16:35:35 +0000

    Create the #new action for a founder and change model validations slightly.

--- BHCDatabase/app/controllers/funders_controller.rb
+++ BHCDatabase/app/controllers/funders_controller.rb
@@ -9,14 +9,28 @@ class FundersController < ApplicationController
   end
 
   def new
+    @funder = Funder.new
   end
 
   def create
+    @funder = Funder.new(funder_params)
+    if @funder.save
+      flash[:success] = 'Created the new funder!'
+      redirect_to @funder
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def funder_params
+    params.require(:funder).permit(:name, :description, :email, :url, :telephone, :address)
   end
 end
