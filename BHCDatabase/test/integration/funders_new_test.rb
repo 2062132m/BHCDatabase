fb033639e06fbcf74323678b86bc54b07ace967c
DaBrown95 <david_a_brown@mac.com>
2017-03-02 16:53:47 +0000

    Add integration test for funder's new action.

---
+++ BHCDatabase/test/integration/funders_new_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class FundersNewTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:david)
+    log_in_as(@user)
+  end
+
+  test 'invalid new funder' do
+    get new_funder_url
+    assert_no_difference 'Funder.count' do
+      post funders_path, params: {funder: {name: '',
+                                           description: Faker::Lorem.sentence,
+                                           email: Faker::Internet.email,
+                                           telephone: Faker::PhoneNumber.phone_number,
+                                           url: Faker::Internet.url,
+                                           address: Faker::Lorem.sentence}}
+    end
+    assert_template 'funders/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test 'valid new funder' do
+    get new_funder_url
+    assert_difference 'Funder.count', 1 do
+      post funders_path, params: {funder: {name: Faker::Company.name,
+                                           description: Faker::Lorem.sentence,
+                                           email: Faker::Internet.email,
+                                           telephone: Faker::PhoneNumber.phone_number,
+                                           url: Faker::Internet.url,
+                                           address: Faker::Lorem.sentence}}
+    end
+    follow_redirect!
+    assert_template 'funders/show'
+  end
+end
