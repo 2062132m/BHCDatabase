3ab095f62d12d89882bc895cd314ea520af60e3b
David Robertson <dmichaelrobertson@gmail.com>
2017-03-07 14:40:45 +0000

    final touches to controller

--- BHCDatabase/app/controllers/users_controller.rb
+++ BHCDatabase/app/controllers/users_controller.rb
@@ -51,11 +51,12 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = 'User updated'
+    unless @user.update_attributes(user_params)
+      flash[:danger] = 'Something went wrong'
       redirect_to @user
     else
-      render 'edit'
+      flash[:success] = 'User updated'
+      redirect_to @user
     end
   end
 
