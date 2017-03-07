81a69878da6aeeac397786e1545a4e6c31e706e5
DaBrown95 <david_a_brown@mac.com>
2017-03-07 14:52:29 +0000

    Update tests to account for lack of sort field.

--- BHCDatabase/app/models/question.rb
+++ BHCDatabase/app/models/question.rb
@@ -2,6 +2,5 @@ class Question < ApplicationRecord
 
   validates :question, presence: true, length: {maximum: 255}
   validates :visible, inclusion: {in: [true, false]}
-  validates :sort, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   validates :multiple_choice, inclusion: {in: [true, false]}
 end
