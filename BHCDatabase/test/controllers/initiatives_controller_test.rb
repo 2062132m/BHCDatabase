197dc06b239b6251ce7aaa3e77024cbd869a171a
DaBrown95 <david_a_brown@mac.com>
2017-03-09 13:03:59 +0000

    Improve controller tests for an initiative.

--- BHCDatabase/test/controllers/initiatives_controller_test.rb
+++ BHCDatabase/test/controllers/initiatives_controller_test.rb
@@ -7,9 +7,30 @@ class InitiativesControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get initiatives_new_url
+    assert_template 'initiatives/new'
     assert_response :success
+  end
+
+  test 'should get index' do
+    get initiatives_url
+    assert_response :success
+    assert_template 'initiatives/index'
+  end
+
+  test 'should get initiative one' do
+    one = initiatives(:one)
+    get initiative_url(one)
+    assert_response :success
+    assert_template 'initiatives/show'
+  end
+
+  test 'should get edit' do
+    one = initiatives(:one)
+    get edit_initiative_url(one)
+    assert_response :success
+    assert_template 'initiatives/edit'
   end
 
 end
