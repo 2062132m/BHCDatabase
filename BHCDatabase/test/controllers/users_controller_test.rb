42bb7f8e29f44f1198242214d4be035da2e6dce7
DaBrown95 <david_a_brown@mac.com>
2017-03-09 11:37:20 +0000

    Add controller tests for 'users.'

--- BHCDatabase/test/controllers/users_controller_test.rb
+++ BHCDatabase/test/controllers/users_controller_test.rb
@@ -1,9 +1,46 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "should get new" do
-  #   get signup_path
-  #   assert_response :success
-  # end
+  def setup
+    @user = users(:david)
+    log_in_as(@user)
+  end
+
+  test 'should get index' do
+    get users_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_user_url
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    service_user = users(:service_user)
+    get edit_user_url(service_user)
+    assert_response :success
+  end
+
+  test 'should get service_user' do
+    service_user = users(:service_user)
+    get user_url(service_user)
+    assert_response :success
+    assert_template 'users/show'
+  end
+
+  test 'should get volunteer' do
+    volunteer = users(:volunteer)
+    get user_url(volunteer)
+    assert_response :success
+    assert_template 'users/show'
+  end
+
+  test 'should get david' do
+    david = users(:david)
+    get user_url(david)
+    assert_response :success
+    assert_template 'users/show'
+  end
 
 end
