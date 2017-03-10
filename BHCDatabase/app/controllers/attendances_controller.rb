beaad2a6c2874491af82d91b2a5dda0669f5d3b0
David Robertson <dmichaelrobertson@gmail.com>
2017-03-10 12:40:53 +0000

    Added comments to attendance controller.

--- BHCDatabase/app/controllers/attendances_controller.rb
+++ BHCDatabase/app/controllers/attendances_controller.rb
@@ -5,9 +5,13 @@ class AttendancesController < ApplicationController
   def create
     meeting_id = 0
     @numAttendees = 0
+    # If attendance form was completed
     unless params[:attendance].nil?
       params[:attendance].each do |a|
+        # Split each entry at the ','
+        # Words is a 2 item array with [user_id, meeting_id]
         words = a.split(',')
+        # Scan the items to get the target information
         @attendance = Attendance.new(:user_id => words[0].scan(/\d+$/).first,
                                      :meeting_id => words[1][0...-1].scan(/\d+$/).first)
         unless @attendance.save
@@ -15,12 +19,17 @@ class AttendancesController < ApplicationController
           redirect_to Meeting.find(meeting_id)
           return
         end
+        # Total the number of attendants
         @numAttendees += 1
+        # Get the meeting_id, which is always the same
         meeting_id = words[1][0...-1].scan(/\d+$/).first
       end
       meeting = Meeting.find(meeting_id)
       @initiative = meeting.initiative
+      # Get the total number of people in the initiative
       @totalAttendees = Enrolment.where(:initiative_id => @initiative, :user_id => (User.where(:privilege => 2).ids)).count
+      # Convert totalAttendees to a float to force floating point division
+      #   in order to calculate the attendance percentage
       meeting.update_attribute(:attendance, @numAttendees/@totalAttendees.to_f * 100)
       meeting.save
       flash[:success] = "Attendance saved!"
