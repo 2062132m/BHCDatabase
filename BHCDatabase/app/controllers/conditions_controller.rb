e2eefef69addb10b990b4febfd907f04fab3a774
David Robertson <dmichaelrobertson@gmail.com>
2017-03-10 13:13:13 +0000

    Add controller logic

--- BHCDatabase/app/controllers/conditions_controller.rb
+++ BHCDatabase/app/controllers/conditions_controller.rb
@@ -1,8 +1,32 @@
 class ConditionsController < ApplicationController
   def new
     @condition = Condition.new
+    @medical_conditions = MedicalCondition.all
+    # get all the users and consolidate into a JSON object
+    respond_to do |format|
+      format.html
+      format.json { render json: @medical_conditions }
+    end
   end
 
   def create
+    # parse values returned to get respective ids
+    # could be an issue if we allow duplicate names
+    @medical_condition = MedicalCondition.where(:name => condition_params[:medical_condition_id]).first
+    # if either is nil, return an empty Enrolment to force an error
+    unless @medical_condition == nil
+      @condition = Condition.new(medical_condition_id: @medical_condition.id,
+                                 condition_params[:user_id])
+    else
+      @condition = Condition.new
+    end
+    if @condition.save
+      flash[:success] = 'Assigned the new condition!'
+      redirect_to @condition.user
+    else
+      flash[:danger] = 'This medical condition does not exist'
+      # redirect back to the form
+      redirect_to :back
+    end
   end
 end
