d97e9813fcbbb2aa7f761c8bb1a437751f22eaeb
David Robertson <dmichaelrobertson@gmail.com>
2017-03-10 13:44:08 +0000

    Fix test.

--- BHCDatabase/test/controllers/conditions_controller_test.rb
+++ BHCDatabase/test/controllers/conditions_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class ConditionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get conditions_new_url
-    assert_response :success
+
+  def setup
+    @user = users(:admin)
+    log_in_as(@user)
   end
 
 end
