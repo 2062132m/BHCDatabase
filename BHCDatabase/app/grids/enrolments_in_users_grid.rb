68dae0b5939392150618cbbe8e252ec813f9725c
David Robertson <dmichaelrobertson@gmail.com>
2017-03-12 18:10:58 +0000

    Add unenrol button to initiatives grid on user page.

--- BHCDatabase/app/grids/enrolments_in_users_grid.rb
+++ BHCDatabase/app/grids/enrolments_in_users_grid.rb
@@ -20,5 +20,9 @@ class EnrolmentsInUsersGrid
       value == model.created_at ? 'Still Enrolled' : value.strftime('%d/%m/%Y - %H:%M')
     end
   end
+  column(:id, :header => 'Unenrol', :mandatory => true) do |model|
+    format(model.id) do |value|
+      link_to "<i class='glyphicon glyphicon-trash'></i> Unenrol".html_safe, service_request_path(value), method: :delete, class: "btn btn-default delete-btn2"
+    end
+  end
 end
-
