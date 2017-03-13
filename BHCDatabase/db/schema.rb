068065ee389eb11e8c2fc275d578c40609f01ed4
David Robertson <dmichaelrobertson@gmail.com>
2017-03-13 12:25:31 +0000

    Create unenrolment model and migrate.

--- BHCDatabase/db/schema.rb
+++ BHCDatabase/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307142657) do
+ActiveRecord::Schema.define(version: 20170313122437) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "feedback_id"
@@ -119,6 +119,15 @@ ActiveRecord::Schema.define(version: 20170307142657) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_service_requests_on_user_id"
+  end
+
+  create_table "unenrolments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "initiative_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["initiative_id"], name: "index_unenrolments_on_initiative_id"
+    t.index ["user_id"], name: "index_unenrolments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
