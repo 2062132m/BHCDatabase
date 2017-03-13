8d3ea9f728a84ed1bc08d1bbeb25fc0a230adf98
DaBrown95 <david_a_brown@mac.com>
2017-03-13 15:29:16 +0000

    Add unique database constraint to an initiative.

--- BHCDatabase/db/schema.rb
+++ BHCDatabase/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313152421) do
+ActiveRecord::Schema.define(version: 20170313152806) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "feedback_id"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170313152421) do
     t.string   "location"
     t.boolean  "archived",        default: false
     t.string   "reason_archived"
+    t.index ["name", "location"], name: "index_initiatives_on_name_and_location", unique: true
   end
 
   create_table "medical_conditions", force: :cascade do |t|
