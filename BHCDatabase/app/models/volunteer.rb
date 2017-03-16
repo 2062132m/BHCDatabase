4ef551f3f4a56f20bfaa8a7783c0159dc65ad94a
David Robertson <dmichaelrobertson@gmail.com>
2017-03-16 15:57:32 +0000

    Add validations to volunteer model

--- BHCDatabase/app/models/volunteer.rb
+++ BHCDatabase/app/models/volunteer.rb
@@ -1,2 +1,12 @@
 class Volunteer < ApplicationRecord
+  validates :life_experiences, presence: true
+  validates :skills, presence: true
+  validates :aspirations, presence: true
+  validates :num_children, presence: true
+  validates :childcare_help, presence: true
+  validates :carer, presence: true
+  validates :carer_costs, presence: true
+  validates :employment, presence: true
+  validates :registered_disabled, presence: true
+  validates :induction_completed, presence: true
 end
