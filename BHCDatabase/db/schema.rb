40a77050bfe67162eed7f988fe5b447224027a0d
DaBrown95 <david_a_brown@mac.com>
2017-03-16 13:45:00 +0000

    Rename 'name' to 'forename' for a user.

--- BHCDatabase/db/schema.rb
+++ BHCDatabase/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316113831) do
+ActiveRecord::Schema.define(version: 20170316134330) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "feedback_id"
@@ -212,7 +212,7 @@ ActiveRecord::Schema.define(version: 20170316113831) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string "forename"
     t.string   "email"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
