1ff7a60dd6167ebf0190b69c7c4bd305d9b8f6d1
DaBrown95 <david_a_brown@mac.com>
2017-03-20 12:36:29 +0000

    Add model validations to a volunteer

--- BHCDatabase/app/models/volunteer.rb
+++ BHCDatabase/app/models/volunteer.rb
@@ -1,12 +1,12 @@
 class Volunteer < ApplicationRecord
-  validates :life_experiences, presence: true
-  validates :skills, presence: true
-  validates :aspirations, presence: true
-  validates :num_children, presence: true
-  validates :childcare_help, presence: true
-  validates :carer, presence: true
-  validates :carer_costs, presence: true
-  validates :employment, presence: true
-  validates :registered_disabled, presence: true
-  validates :induction_completed, presence: true
+  validates :life_experiences, presence: true, length: {maximum: 65536}
+  validates :skills, presence: true, length: {maximum: 65536}
+  validates :aspirations, presence: true, length: {maximum: 65536}
+  validates :num_children, presence: true, numericality: {only_integer: true}
+  validates :childcare_help, presence: true, inclusion: {in: [true, false]}
+  validates :carer, presence: true, inclusion: {in: [true, false]}
+  validates :carer_costs, presence: true, inclusion: {in: [true, false]}
+  validates :employment, presence: true, length: {maximum: 255}
+  validates :registered_disabled, presence: true, inclusion: {in: [true, false]}
+  validates :induction_completed, presence: true, inclusion: {in: [true, false]}
 end
