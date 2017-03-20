885a10a5b0a67558cd60b5a0645d4fa5b9f1addf
DaBrown95 <david_a_brown@mac.com>
2017-03-20 12:12:11 +0000

    Change max lenght for an answer

--- BHCDatabase/app/models/answer.rb
+++ BHCDatabase/app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :feedback, required: false
   belongs_to :question
 
-  validates :response, presence: true, length: {maximum: 255}
+  validates :response, presence: true, length: {maximum: 65536}
   validates_uniqueness_of :feedback_id, :scope => :question_id
 
   def <=>(other)
