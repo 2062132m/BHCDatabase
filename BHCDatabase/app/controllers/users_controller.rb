9201fad166a9938adaef1169d906c3cc4e755b4c
David Robertson <dmichaelrobertson@gmail.com>
2017-03-23 12:32:21 +0000

    Allow a user being created as volunteer to have a feedback_due date.

--- BHCDatabase/app/controllers/users_controller.rb
+++ BHCDatabase/app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.reg_date &&= Date.today
     @user.known_as &&= @user.forename
-    @user.feedback_due = @user.privilege == 2 ? Date.today : nil
+    @user.feedback_due = @user.privilege > 0 ? Date.today : nil
     if @user.save
       if @user.privilege == 1
         flash[:success] = 'Successfully signed up new volunteer! Please fill in their volunteer details using the button below.'
