60334b0604e78fb9693f953edf0a65a9bb4de329
DaBrown95 <david_a_brown@mac.com>
2017-03-23 18:08:21 +0000

    Remove redundant error to log in.

--- BHCDatabase/app/controllers/application_controller.rb
+++ BHCDatabase/app/controllers/application_controller.rb
@@ -15,7 +15,6 @@ class ApplicationController < ActionController::Base
   def require_login
     unless current_user
       redirect_to login_path
-      flash[:danger] = 'Please log in.'
     end
   end
 
