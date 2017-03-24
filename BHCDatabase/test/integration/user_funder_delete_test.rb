80c5ba1be42da96e9e7fba2ceafcde64764da390
David Robertson <dmichaelrobertson@gmail.com>
2017-03-24 17:34:41 +0000

    Add tests for adding and removing user funding.

---
+++ BHCDatabase/test/integration/user_funder_delete_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UserFunderDeleteTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin = users(:admin)
+    @user = users(:service_user)
+    @funder_one = funders(:one)
+    @funder_two = funders(:two)
+    log_in_as(@admin)
+  end
+
+  test "delete user funder test" do
+    get user_path(@user)
+    assert_difference 'UserFunder.count', -1 do
+      get remove_user_funding_funder_path(@funder_two), params: { user_id: @user.id }
+    end
+    follow_redirect!
+    assert_not flash.empty?
+    assert_template 'users/show'
+  end
+end
