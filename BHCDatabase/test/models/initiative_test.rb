4d16f056d69e4e2bec015f652de427f7d83465ff
DaBrown95 <david_a_brown@mac.com>
2017-03-24 17:50:12 +0000

    Refactor initiative model test.

--- BHCDatabase/test/models/initiative_test.rb
+++ BHCDatabase/test/models/initiative_test.rb
@@ -1,48 +1,52 @@
 require 'test_helper'
 
+# InitiativeTest model contains the model tests for an Initiative
 class InitiativeTest < ActiveSupport::TestCase
   def setup
-    @area = Area.create(name: "Example Area", description: "Example description")
-    @initiative = Initiative.new(name: "Example Initiative",
-    description: "Example description.", area_id: @area.id, location: "TestTest")
+    @area = Area.create(name: 'Example Area',
+                        description: 'Example description')
+    @initiative = Initiative.new(name: 'Example Initiative',
+                                 description: 'Example description.',
+                                 area_id: @area.id,
+                                 location: 'TestTest')
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @initiative.valid?
   end
 
-  test "name should be present" do
-    @initiative.name = ""
+  test 'name should be present' do
+    @initiative.name = ''
     assert_not @initiative.valid?
   end
 
-  test "description should be present" do
-    @initiative.description = ""
+  test 'description should be present' do
+    @initiative.description = ''
     assert_not @initiative.valid?
   end
 
-  test "location should be present" do
-    @initiative.location = ""
+  test 'location should be present' do
+    @initiative.location = ''
     assert_not @initiative.valid?
   end
 
-  test "area_id should be present" do
+  test 'area_id should be present' do
     @initiative.area_id = nil
     assert_not @initiative.valid?
   end
 
-  test "location should not be too long" do
-    @initiative.location = "a" * 65537
+  test 'location should not be too long' do
+    @initiative.location = 'a' * 65_537
     assert_not @initiative.valid?
   end
 
-  test "name should not be too long" do
-    @initiative.name = "a" * 256
+  test 'name should not be too long' do
+    @initiative.name = 'a' * 256
     assert_not @initiative.valid?
   end
 
-  test "description should not be too long" do
-    @initiative.description = "a" * 65537
+  test 'description should not be too long' do
+    @initiative.description = 'a' * 65_537
     assert_not @initiative.valid?
   end
 end
