c2014db55957a54722f15ade3180ca4a43a9dae8
DaBrown95 <david_a_brown@mac.com>
2017-03-24 15:11:30 +0000

    Comment and refactor feedback.

--- BHCDatabase/app/models/feedback.rb
+++ BHCDatabase/app/models/feedback.rb
@@ -1,5 +1,9 @@
+# Feedback allows us to link a user to a specific feedback they have left.
+# This model is used by answers to discern who the answer came from and also
+#   what feedback it belongs to.
 class Feedback < ApplicationRecord
   belongs_to :user
+
   has_many :answers, dependent: :delete_all
 
   accepts_nested_attributes_for :answers
