d48a0f0515bcab3f5982c48f59268eaee12365c4
DaBrown95 <david_a_brown@mac.com>
2017-03-24 17:31:00 +0000

    Write model tests for an feedback.

--- BHCDatabase/test/models/feedback_test.rb
+++ BHCDatabase/test/models/feedback_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
+# FeedbackTest is the model tests for our Feedback model
 class FeedbackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @feedback = feedbacks(:one)
+  end
+
+  test 'should be valid' do
+    assert @feedback.valid?
+  end
+
+  test 'should have a user' do
+    @feedback.user = nil
+    assert @feedback
+  end
 end
