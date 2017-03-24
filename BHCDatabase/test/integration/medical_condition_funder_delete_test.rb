ba24703e92b9409d9221590024414a7f73c906d0
David Robertson <dmichaelrobertson@gmail.com>
2017-03-24 17:00:58 +0000

    Add tests for adding and removing medical conditions fundings.

---
+++ BHCDatabase/test/integration/medical_condition_funder_delete_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class MedicalConditionFunderDeleteTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin = users(:admin)
+    @medical_condition = medical_conditions(:one)
+    @funder_one = funders(:one)
+    @funder_two = funders(:two)
+    log_in_as(@admin)
+  end
+
+  test "delete medical condition funder test" do
+    get medical_condition_path(@medical_condition)
+    assert_difference 'MedicalConditionFunder.count', -1 do
+      get remove_medical_funding_funder_path(@funder_two), params: { medical_condition_id: @medical_condition.id }
+    end
+    follow_redirect!
+    assert_not flash.empty?
+    assert_template 'medical_conditions/show'
+  end
+end
