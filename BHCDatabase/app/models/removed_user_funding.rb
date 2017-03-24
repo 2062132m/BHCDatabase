5754e4cecc283875eed92ca242d79112b4d4ab42
DaBrown95 <david_a_brown@mac.com>
2017-03-24 15:15:45 +0000

    Comment and refactor removed_user_funding.

--- BHCDatabase/app/models/removed_user_funding.rb
+++ BHCDatabase/app/models/removed_user_funding.rb
@@ -1,3 +1,5 @@
+# RemovedUserFunding holds the history of given user having been funded in the
+#   past by a particular Funder.
 class RemovedUserFunding < ApplicationRecord
   belongs_to :funder
   belongs_to :user
