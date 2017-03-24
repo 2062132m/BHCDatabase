3cf5f97f0ea438820b1232672c8b0b6cc995060a
DaBrown95 <david_a_brown@mac.com>
2017-03-24 15:11:50 +0000

    Comment and refactor a funder.

--- BHCDatabase/app/models/funder.rb
+++ BHCDatabase/app/models/funder.rb
@@ -1,4 +1,7 @@
+# Funder is the generic model that holds information for a given Funder.
 class Funder < ApplicationRecord
+  # has_many ... through: allows for the quick querying of Funders. It allows,
+  #   for example user.funders to return all the funders for a given user.
   has_many :initiative_funders, dependent: :delete_all
   has_many :initiatives, through: :initiative_funders
   has_many :medical_condition_funders, dependent: :delete_all
@@ -6,15 +9,19 @@ class Funder < ApplicationRecord
   has_many :user_funders, dependent: :delete_all
   has_many :users, through: :user_funders
 
-  validates :name, presence: true, length: {maximum: 255}, uniqueness: {:scope => :email, case_sensitive: false}
+  validates :name,
+            presence: true,
+            length: { maximum: 255 },
+            uniqueness: { scope: :email, case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, length: {maximum: 255},
-            format: {with: VALID_EMAIL_REGEX},
-            uniqueness: {case_sensitive: false},
+  validates :email,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false },
             allow_blank: true
-  validates :telephone, length: {maximum: 255}, allow_blank: true
-  validates :address, length: {maximum: 255}, allow_blank: true
-  validates :url, length: {maximum: 255}
-  validates_format_of :url, :with => URI::regexp(%w(http https)), allow_blank: true
-  validates :description, length: {maximum: 65536}, allow_blank: true
+  validates :telephone, length: { maximum: 255 }, allow_blank: true
+  validates :address, length: { maximum: 255 }, allow_blank: true
+  validates :url, length: { maximum: 255 }
+  validates_format_of :url, with: URI.regexp(%w(http https)), allow_blank: true
+  validates :description, length: { maximum: 65_536 }, allow_blank: true
 end
