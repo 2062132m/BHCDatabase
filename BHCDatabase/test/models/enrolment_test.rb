f52a115b97f7729254b160b6855448416588d012
DaBrown95 <david_a_brown@mac.com>
2017-03-24 17:26:32 +0000

    Write model tests for an enrolments.

--- BHCDatabase/test/models/enrolment_test.rb
+++ BHCDatabase/test/models/enrolment_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
+# EnrolmentTest is the generic model test for enrolments
 class EnrolmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @enrolment = enrolments(:one)
+  end
+
+  test 'should be valid' do
+    assert @enrolment.valid?
+  end
+
+  test 'should have a user' do
+    @enrolment.user = nil
+    assert_not @enrolment.valid?
+  end
+
+  test 'should have an initiative' do
+    @enrolment.initiative = nil
+    assert_not @enrolment.valid?
+  end
 end
