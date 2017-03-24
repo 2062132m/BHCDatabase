fc6188df34b212c9f663db7cf5430e4e929f0fc8
DaBrown95 <david_a_brown@mac.com>
2017-03-24 15:13:23 +0000

    Comment and refactor a medical_condition.

--- BHCDatabase/app/models/medical_condition.rb
+++ BHCDatabase/app/models/medical_condition.rb
@@ -1,13 +1,18 @@
+# MedicalCondition is the model that defines a generic medical condition.
 class MedicalCondition < ApplicationRecord
-  validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 255}
-  validates :description, presence: true, length: {maximum: 65536}
-  validates :reason_archived, length: {maximum: 255}
-
   has_many :conditions, dependent: :delete_all
   has_many :unassigned_conditions, dependent: :delete_all
   has_many :users, through: :conditions
   has_many :medical_condition_funders, dependent: :delete_all
+  has_many :funders, through: :medical_condition_funders
   has_many :removed_medical_fundings, dependent: :delete_all
+
+  validates :name,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 65_536 }
+  validates :reason_archived, length: { maximum: 255 }
 
   def archived?
     archived
