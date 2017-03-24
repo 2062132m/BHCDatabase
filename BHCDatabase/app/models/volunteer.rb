a494d07913374a691a76fe280c6a413f64f68da6
DaBrown95 <david_a_brown@mac.com>
2017-03-24 15:17:34 +0000

    Comment and refactor volunteer.

--- BHCDatabase/app/models/volunteer.rb
+++ BHCDatabase/app/models/volunteer.rb
@@ -1,12 +1,13 @@
+# Volunteer is the generic model that holds the 'PDP' for a given volunteer.
 class Volunteer < ApplicationRecord
-  validates :life_experiences, presence: true, length: {maximum: 65536}
-  validates :skills, presence: true, length: {maximum: 65536}
-  validates :aspirations, presence: true, length: {maximum: 65536}
-  validates :num_children, presence: true, numericality: {only_integer: true}
-  validates :childcare_help, inclusion: {in: [true, false]}
-  validates :carer, inclusion: {in: [true, false]}
-  validates :carer_costs, inclusion: {in: [true, false]}
-  validates :employment, presence: true, length: {maximum: 255}
-  validates :registered_disabled, inclusion: {in: [true, false]}
-  validates :induction_completed, inclusion: {in: [true, false]}
+  validates :life_experiences, presence: true, length: { maximum: 65_536 }
+  validates :skills, presence: true, length: { maximum: 65_536 }
+  validates :aspirations, presence: true, length: { maximum: 65_536 }
+  validates :num_children, presence: true, numericality: { only_integer: true }
+  validates :childcare_help, inclusion: { in: [true, false] }
+  validates :carer, inclusion: { in: [true, false] }
+  validates :carer_costs, inclusion: { in: [true, false] }
+  validates :employment, presence: true, length: { maximum: 255 }
+  validates :registered_disabled, inclusion: { in: [true, false] }
+  validates :induction_completed, inclusion: { in: [true, false] }
 end
