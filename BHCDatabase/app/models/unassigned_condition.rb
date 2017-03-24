6546572e78b930df2575efb0735b476eb8efa245
DaBrown95 <david_a_brown@mac.com>
2017-03-24 15:16:15 +0000

    Comment and refactor unassigned_condition.

--- BHCDatabase/app/models/unassigned_condition.rb
+++ BHCDatabase/app/models/unassigned_condition.rb
@@ -1,3 +1,5 @@
+# UnassignedCondition holds the history of a given user having a medical
+#   history.
 class UnassignedCondition < ApplicationRecord
   belongs_to :medical_condition
   belongs_to :user
