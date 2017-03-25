1c7ada82430d1e49e941f55dc571495b25b8c156
DaBrown95 <david_a_brown@mac.com>
2017-03-25 20:42:31 +0000

    Fix feedback_new_test from failing.

--- BHCDatabase/test/integration/feedback_new_test.rb
+++ BHCDatabase/test/integration/feedback_new_test.rb
@@ -5,21 +5,20 @@ class FeedbackNewTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:admin)
     @service_user = users(:service_user)
+    @question_one = questions(:one)
+    @question_two = questions(:two)
     @service_user_two = users(:service_user2)
     @feedback = feedbacks(:two)
     log_in_as(@admin)
   end
 
-  test "valid new feedback test" do
+  test 'valid new feedback test' do
     get new_feedback_path, params: { user_id: @service_user.id }
+    answers = { '0' => { question_id: @question_one.id, response: 'example' },
+                '1' => { question_id: @question_two.id, response: 'example' } }
     assert_difference 'Feedback.count', 1 do
       post feedbacks_path, params: { feedback: { user_id: @service_user.id,
-                                     answers_attributes: {
-                                       '0': { question_id: 1, response: "example" },
-                                       '1': { question_id: 2, response: "example" },
-                                       }
-                                     }
-                                   }
+                                                 answers_attributes: answers }}
     end
     follow_redirect!
     assert_not flash.empty?
@@ -27,7 +26,7 @@ class FeedbackNewTest < ActionDispatch::IntegrationTest
   end
 
   # Ensure you can only view your own feedback
-  test "access other feedback show" do
+  test 'access other feedback show' do
     log_in_as(@service_user)
     get feedback_path(@feedback)
     follow_redirect!
