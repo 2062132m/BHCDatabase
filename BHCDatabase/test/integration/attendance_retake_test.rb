52ea8afeee2739f6a1128845103036b02b4cf041
DaBrown95 <david_a_brown@mac.com>
2017-03-25 20:39:56 +0000

    Fix attendance_retake_test from failing.

--- BHCDatabase/test/integration/attendance_retake_test.rb
+++ BHCDatabase/test/integration/attendance_retake_test.rb
@@ -5,18 +5,19 @@ class AttendancesRetakeTest < ActionDispatch::IntegrationTest
     @admin = users(:admin)
     @volunteer = users(:volunteer)
     @service_user = users(:service_user)
-    @meeting = meetings(:one)
+    @meeting = meetings(:two)
     log_in_as(@admin)
   end
 
   # Ensure attendances are deleted and attendance percentage is reset to 0
   #   upon 'retaking' the attendance
-  test "retake attendance test" do
+  test 'retake attendance test' do
     # Start by creating an attendance
     get meeting_path(@meeting)
     assert_difference 'Attendance.count', 2 do
       # Assume volunteer and service_user are attending
-      post attendances_path, params: { meeting_id: @meeting.id, attendance: [@volunteer.id, @service_user.id] }
+      post attendances_path, params: { meeting_id: @meeting.id,
+                                       attendance: [@volunteer.id, @service_user.id] }
     end
     # Arbitrary value to avoid expected nil attendance error
     @meeting.update_attributes(attendance: 50)
