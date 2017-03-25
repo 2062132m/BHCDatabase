f85113d64ec2a2db14e3594a0b886810f2a1bf37
DaBrown95 <david_a_brown@mac.com>
2017-03-25 20:40:43 +0000

    Fix condition_new_test from failing.

--- BHCDatabase/test/integration/condition_new_test.rb
+++ BHCDatabase/test/integration/condition_new_test.rb
@@ -4,16 +4,17 @@ class ConditionNewTest < ActionDispatch::IntegrationTest
 
   def setup
     @admin = users(:admin)
-    @service_user = users(:service_user)
+    @service_user = users(:service_user2)
     @medical_condition_one = medical_conditions(:one)
     @medical_condition_two = medical_conditions(:two)
     log_in_as(@admin)
   end
 
-  test "valid new condition test" do
+  test 'valid new condition test' do
     get new_condition_path, params: { user_id: @service_user.id }
     assert_difference 'Condition.count', 1 do
-      post conditions_path, params: { condition: { user_id: @service_user.id, medical_condition_id: @medical_condition_one.name } }
+      post conditions_path, params: { condition: { user_id: @service_user.id,
+                                                   medical_condition_id: @medical_condition_one.name } }
     end
     follow_redirect!
     assert_not flash.empty?
@@ -21,10 +22,11 @@ class ConditionNewTest < ActionDispatch::IntegrationTest
   end
 
   # Where specified medical condition doesn't exist
-  test "invalid new condition test" do
+  test 'invalid new condition test' do
     get new_condition_path, params: { user_id: @service_user.id }
     assert_no_difference 'Condition.count' do
-      post conditions_path, params: { condition: { user_id: @service_user.id, medical_condition_id: '' } }
+      post conditions_path, params: { condition: { user_id: @service_user.id,
+                                                   medical_condition_id: '' } }
     end
     follow_redirect!
     assert_not flash.empty?
@@ -32,10 +34,11 @@ class ConditionNewTest < ActionDispatch::IntegrationTest
   end
 
   # Where user already has specified medical condition
-  test "duplicate new condition test" do
+  test 'duplicate new condition test' do
     get new_condition_path, params: { user_id: @service_user.id }
     assert_no_difference 'Condition.count' do
-      post conditions_path, params: { condition: { user_id: @service_user.id, medical_condition_id: @medical_condition_two.name } }
+      post conditions_path, params: { condition: { user_id: @service_user.id,
+                                                   medical_condition_id: @medical_condition_two.name } }
     end
     follow_redirect!
     assert_not flash.empty?
