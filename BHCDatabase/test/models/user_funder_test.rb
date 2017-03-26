6b2207eb4d788fcd28135bb99d48be5596062cde
DaBrown95 <david_a_brown@mac.com>
2017-03-26 23:08:48 +0100

    Add model tests for a user funder.

--- BHCDatabase/test/models/user_funder_test.rb
+++ BHCDatabase/test/models/user_funder_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
+# UserFunderTest is the model test for a generic user funder.
 class UserFunderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user_funder = user_funders(:two)
+  end
+
+  test 'should be valid' do
+    assert @user_funder.valid?
+  end
+
+  test 'funder should be present' do
+    @user_funder.funder = nil
+    assert_not @user_funder.valid?
+  end
+
+  test 'user should be present' do
+    @user_funder.user = nil
+    assert_not @user_funder.valid?
+  end
+
+  test 'index on user and funder' do
+    @duplicate_funder = @user_funder.dup
+    assert_not @duplicate_funder.valid?
+    assert_no_difference 'UserFunder.count' do
+      @duplicate_funder.save
+    end
+  end
 end
