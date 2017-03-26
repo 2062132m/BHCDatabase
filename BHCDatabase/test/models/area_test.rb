42ff5fbb1547b5e45a40a6f8b5c6b8b6ebd0c750
DaBrown95 <david_a_brown@mac.com>
2017-03-26 19:17:40 +0100

    Add test for index on name for an area.

--- BHCDatabase/test/models/area_test.rb
+++ BHCDatabase/test/models/area_test.rb
@@ -1,31 +1,42 @@
 require 'test_helper'
 
+# AreaTest is the generic model test for an area.
 class AreaTest < ActiveSupport::TestCase
   def setup
-    @area = Area.new(name: "Example Area", description: "Example description.")
+    @area = areas(:one)
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @area.valid?
   end
 
-  test "name should be present" do
-    @area.name = ""
+  test 'name should be present' do
+    @area.name = ''
     assert_not @area.valid?
   end
 
-  test "description should be present" do
-    @area.description = ""
+  test 'description should be present' do
+    @area.description = ''
     assert_not @area.valid?
   end
 
-  test "name should not be too long" do
-    @area.name = "a" * 256
+  test 'name should not be too long' do
+    @area.name = 'a' * 256
     assert_not @area.valid?
   end
 
-  test "description should not be too long" do
-    @area.description = "a" * 65537
+  test 'description should not be too long' do
+    @area.description = 'a' * 65_537
     assert_not @area.valid?
+  end
+
+  test 'index on name' do
+    @area2 = Area.new(name: @area.name, description: 'Fake description')
+    assert_not @area2.description == @area.description
+    assert @area2.name == @area.name
+    assert_not @area2.valid?
+    assert_no_difference 'Area.count' do
+      @area2.save
+    end
   end
 end
