80d3a028914005561eb3daa725927b6ff9ff4b09
DaBrown95 <david_a_brown@mac.com>
2017-03-26 22:24:34 +0100

    Add model tests for a service request.

--- BHCDatabase/test/models/service_request_test.rb
+++ BHCDatabase/test/models/service_request_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
+# ServiceRequest test is the generic model test for a service request.
 class ServiceRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @service_request = service_requests(:one)
+  end
+
+  test 'should be valid' do
+    @service_request.valid?
+  end
+
+  test 'request should be present' do
+    @service_request.request = ''
+    assert_not @service_request.valid?
+  end
+
+  test 'request should not be too long' do
+    @service_request.request = 'a' * 65_537
+    assert_not @service_request.valid?
+  end
+
+  test 'should have a user' do
+    @service_request.user = nil
+    assert_not @service_request.valid?
+  end
+
+  test 'allows multiple requests from a user' do
+    @duplicate_request = @service_request.dup
+    @duplicate_request.request = 'My new request'
+    assert_not @duplicate_request.request == @service_request.request
+    assert @duplicate_request.valid?, @duplicate_request.errors.full_messages
+    assert_difference 'ServiceRequest.count', 1 do
+      @duplicate_request.save
+    end
+  end
 end
