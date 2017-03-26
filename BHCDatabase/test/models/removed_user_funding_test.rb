b8565742f1adcfd0a190b22e93de878a19969f66
DaBrown95 <david_a_brown@mac.com>
2017-03-26 22:12:21 +0100

    Add model tests for a removed user funder.

--- BHCDatabase/test/models/removed_user_funding_test.rb
+++ BHCDatabase/test/models/removed_user_funding_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
+# RemovedUserFundingTest is the generic model test for a removed user funder.
 class RemovedUserFundingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @removed_user_funder = removed_user_fundings(:one)
+  end
+
+  test 'should be valid' do
+    assert @removed_user_funder.valid?
+  end
+
+  test 'datetime should accept valid dates' do
+    valid_date = '01/01/2012 23:59:59'
+    @removed_user_funder.date_given = valid_date
+    assert @removed_user_funder.date_given.to_datetime.is_a?(DateTime)
+  end
+
+  test 'datetime should reject invalid dates' do
+    invalid_date = '12/13/2016'
+    @removed_user_funder.date_given = invalid_date
+    assert_raises(Exception) { @removed_user_funder.date_given.to_datetime }
+  end
+
+  test 'should accept duplicate entries' do
+    @duplicate_removed_funder = @removed_user_funder.dup
+    assert @duplicate_removed_funder.valid?
+    assert_difference 'RemovedUserFunding.count', 1 do
+      @duplicate_removed_funder.save
+    end
+  end
 end
